7d578229ab0db5e988ed17ed37b4fd715ee3fac6
sjpbailey <sjpbailey1961@gmail.com>
2023-12-27 22:02:48 -0800

    revise RPi code to send un-split data

--- Code_RPi/talktoarduino.py
+++ Code_RPi/talktoarduino.py
@@ -1,27 +1,37 @@
 import serial
 import socket
 import sys
-
+buffersize=1024
 
 if __name__ == '__main__':
+    ## Read Arduino Data
     ser = serial.Serial('/dev/ttyACM0' ,9600, timeout=10)
     ser.flush
     
-    DestainationIP = '192.168.1.164'
+    ## IP Address to where you want to send the Data
+    DestainationIP = '192.168.1.122'
     SocketPort = 10000
     
+    ## Socket to Client where data is sent
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = (DestainationIP, SocketPort)
+    server_address = ((DestainationIP, SocketPort))
     
+    print('Server Up')
+    
+    
+    ## Future Requests from Client
+    """message1.address=sock.recvfrom(buffersize)
+    message1=message.decode('utf-8')
+    print(message1)
+    print('Client',address[0])"""
+    
+    ## Messages to send to Client GPM, GPM Total
     while True:
-        line = ser.readline().decode('utf8').rstrip()
-        print(line)
-        #print(type(line))
-        #for i in line:
-            #print(i)
-         #   outputgpm=i
-          #  print(outputgpm)
-        
-        
-        sock.sendto(line.encode('utf-8'),server_address)
-        
+        ## Read GPM from Arduino
+        flowGPM = None
+        flowGPM = ser.readline().decode('utf-8').rstrip()
+        ## Send GPM to Server
+        sock.sendto(flowGPM.encode('utf-8'),server_address)
+        print(flowGPM)
+        pass
+    
