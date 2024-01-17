4456cb04191930afd86cc8b84fcaa27e2f0a1c99
sjpbailey <sjpbailey1961@gmail.com>
2024-01-16 21:38:53 -0800

    Update GPMController.py

--- GPMController.py
+++ GPMController.py
@@ -50,7 +50,7 @@ class GPMController(udi_interface.Node):
 
         def set_speed(self, command):
             speed = float(command.get('value'))
-        if speed < -10 or speed > 11:
+        if speed < -.10 or speed > .11:
             LOGGER.error('Invalid volts selection {}'.format(speed))
         else:
             self.setDriver('GV9', speed)
