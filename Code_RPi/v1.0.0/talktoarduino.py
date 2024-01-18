04668cb694227caee18780ef570f61f1d5224bb7
sjpbailey <sjpbailey1961@gmail.com>
2024-01-18 09:25:21 -0800

    Update talktoarduino.py

--- Code_RPi/v1.0.0/talktoarduino.py
+++ Code_RPi/v1.0.0/talktoarduino.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     
     ## IP Address to where you want to send the Data
     DestainationIP = '192.168.1.122'
-    SocketPort = 10000
+    SocketPort = 10000 
+    SocketPort = 10001
     
     ## Socket to Client where data is sent
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
