ce6f32fbc2948d9309290d124635798e0a76d32b
sjpbailey <sjpbailey1961@gmail.com>
2024-01-18 13:52:26 -0800

    Update GPMController.py

--- GPMController.py
+++ GPMController.py
@@ -88,28 +88,29 @@ class GPMController(udi_interface.Node):
             
             psigo = dataArray[2]
             LOGGER.info("PSI input from Socket Server")
-            #LOGGER.info(psigo)
+            LOGGER.info(psigo)
+            
             #LOGGER.info(type(psigo))
             #LOGGER.info(float(psigo))
             psi0 = [float(x) for x in psigo.split()]
-            LOGGER.info("PSI float out to be added")
+            LOGGER.info("Converted PSI input to float, to be added")
             LOGGER.info(psi0)
             
             #for i in psigo:
             #    LOGGER.info(i)
             
             spd1 = self.getDriver('GV9')
+            LOGGER.info("Input from Calibration, Passed GV9 input")
             LOGGER.info(spd1)
-            LOGGER.info("Input from Calibration Passed GV9 input")
             
             #LOGGER.info(type(spd1))
             #LOGGER.info(float(spd1))
             #psi1 = [float(x) for x in spd1.split()]
+            #LOGGER.info(spd1)
             
-            LOGGER.info(spd1)
-            psitotal = psi0 = spd1
-            LOGGER.info(psitotal)
+            psitotal = psi0 + spd1
             LOGGER.info("Added Calibration and PSI Output to GV3")
+            LOGGER.info(psitotal)
             
             #for i in spd1:
             #    LOGGER.info(i)
