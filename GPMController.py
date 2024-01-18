c4687eb752d4c7c10f4fa41ea30be15e688c625a
sjpbailey <sjpbailey1961@gmail.com>
2024-01-18 10:25:32 -0800

    Update GPMController.py

--- GPMController.py
+++ GPMController.py
@@ -57,7 +57,7 @@ class GPMController(udi_interface.Node):
             self.setDriver('GV9', speed/10)
             LOGGER.info('Calibration = ' + str(speed/10) + 'INT')
         
-        spd1 = self.poly.getNodes(speed)
+        spd1 = self.getNodes(speed)
         LOGGER.info("SPEED!!")
         LOGGER.info(spd1)
         """speed = speed/10
