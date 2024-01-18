62c592d65a399f08d72999e5e9b6da100582aa68
sjpbailey <sjpbailey1961@gmail.com>
2024-01-17 22:17:12 -0800

    Update GPMController.py

--- GPMController.py
+++ GPMController.py
@@ -92,7 +92,9 @@ class GPMController(udi_interface.Node):
         self.speed = speed/10
         LOGGER.info("SPEED")
         LOGGER.info(self.speed)
-        self.setDriver('GV10', 'GV3' + 'GV9')
+        speed1 = 'GV3'
+        
+        self.setDriver('GV10', speed1 + speed)
         return self.speed
         
         """for i in res:    #res = [int(i) for i in 'GV9'.split() if i.isdigit()]    
